2aa69e93b2a64f3325ae7a8ce2afc3167527126c
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2019-10-30 06:58:23 +0600

    Update SegmentTree._Lazy.cpp

--- SegmentTree._Lazy.cpp
+++ SegmentTree._Lazy.cpp
@@ -28,13 +28,16 @@ void build_tree( int node, int a, int b ) //node=current node, a-b=current range
 
 ll query_tree( int node, int a, int b, int i, int j )
 {
+    int left = node << 1 ;
+    int right = ( node << 1)+ 1 ;
+    int mid = ( a + b ) / 2 ;
     if( lazy[node]!=0)
     {
         tree[node]+=((b-a+1)*lazy[node]); // Update it
         if(a!=b) //not leaf node.mark it's child as lazy
         {
-            lazy[node<<1] += lazy[node]; //mark child as lazy
-            lazy[(node<<1)+1]+=lazy[node]; //mark child as lazy
+            lazy[left] += lazy[node]; //mark child as lazy
+            lazy[right] += lazy[node]; //mark child as lazy
         }
         lazy[node]=0; // Reset it
     }
@@ -42,9 +45,7 @@ ll query_tree( int node, int a, int b, int i, int j )
         return 0 ; // Out of range
     if(a>=i && b <= j)
         return tree[node];
-    int left = node << 1 ;
-    int right = ( node << 1)+ 1 ;
-    int mid = ( a + b ) / 2 ;
+
     ll q1 = query_tree(left, a, mid, i, j ) ;
     ll q2 = query_tree(right, mid+1, b, i, j ) ;
     return q1+q2 ; // Return final result
@@ -52,13 +53,16 @@ ll query_tree( int node, int a, int b, int i, int j )
 
 void update_tree( int node, int a, int b, int i, int j,ll newValue)
 {
+    int left = node << 1 ;
+    int right = ( node << 1)+ 1 ;
+    int mid = ( a + b ) / 2 ;
     if( lazy[node] != 0 )  //This node needs to be updated
     {
         tree[node] += ((b-a+1)*lazy[node]) ; //update it
         if( a!=b )
         {
-            lazy[node<<1] += lazy[node]; //mark child as lazy
-            lazy[(node<<1)+1]+=lazy[node]; //mark child as lazy
+            lazy[left] += lazy[node]; //mark child as lazy
+            lazy[right] += lazy[node]; //mark child as lazy
         }
         lazy[node]=0; //Reset it
     }
@@ -69,14 +73,11 @@ void update_tree( int node, int a, int b, int i, int j,ll newValue)
         tree[node]+=((b-a+1)*newValue);
         if(a!=b)// Not leaf node
         {
-            lazy[node<<1] += newValue ;
-            lazy[(node<<1)+1] += newValue ;
+            lazy[left] += newValue ;
+            lazy[right] += newValue ;
         }
         return ;
     }
-    int left = node << 1 ;
-    int right = ( node << 1 )+ 1 ;
-    int mid = ( a + b ) / 2 ;
     update_tree( left, a, mid, i, j, newValue ) ;
     update_tree( right, mid+1, b, i, j, newValue ) ;
     tree[node]=tree[left]+tree[right];
