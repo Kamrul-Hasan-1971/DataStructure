99382c9f1d8907f513d5d41f7696f33fdd8e413f
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-11-08 13:12:09 +0600

    Update linked_list_merge_sort_nlogn &_constant_space.cpp

--- linked_list_merge_sort_nlogn &_constant_space.cpp
+++ linked_list_merge_sort_nlogn &_constant_space.cpp
@@ -1,77 +1,33 @@
 class Solution
 {
 public:
-    ListNode *sortList(ListNode *head)
+    ListNode* merge(ListNode* l1, ListNode* l2)
     {
-        if(!head || !(head->next)) return head;
-
-        //get the linked list's length
-        ListNode* cur = head;
-        int length = 0;
-        while(cur)
+        if(!l1 || !l2) return l1?l1:l2;
+        if(l1->val<=l2->val)
         {
-            length++;
-            cur = cur->next;
+            l1->next=merge(l1->next,l2);
+            return l1;
         }
-
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode *left, *right, *tail;
-        for(int step = 1; step < length; step <<= 1)
+        else
         {
-            cur = dummy.next;
-            tail = &dummy;
-            while(cur)
-            {
-                left = cur;
-                right = split(left, step);
-                cur = split(right,step);
-                tail = merge(left, right, tail);
-            }
+            l2->next=merge(l1,l2->next);
+            return l2;
         }
-        return dummy.next;
     }
-private:
-    /**
-     * Divide the linked list into two lists,
-     * while the first list contains first n ndoes
-     * return the second list's head
-     */
-    ListNode* split(ListNode *head, int n)
+    ListNode* sortList(ListNode* head)
     {
-        //if(!head) return NULL;
-        for(int i = 1; head && i < n; i++) head = head->next;
-
-        if(!head) return NULL;
-        ListNode *second = head->next;
-        head->next = NULL;
-        return second;
-    }
-    /**
-      * merge the two sorted linked list l1 and l2,
-      * then append the merged sorted linked list to the node head
-      * return the tail of the merged sorted linked list
-     */
-    ListNode* merge(ListNode* l1, ListNode* l2, ListNode* head)
-    {
-        ListNode *cur = head;
-        while(l1 && l2)
+        if(head==nullptr || head->next==nullptr) return head;
+        ListNode *slow = head,*fast=head->next->next;
+        while(fast&&fast->next)
         {
-            if(l1->val > l2->val)
-            {
-                cur->next = l2;
-                cur = l2;
-                l2 = l2->next;
-            }
-            else
-            {
-                cur->next = l1;
-                cur = l1;
-                l1 = l1->next;
-            }
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        cur->next = (l1 ? l1 : l2);
-        while(cur->next) cur = cur->next;
-        return cur;
+        ListNode *left,*right;
+        right = sortList(slow->next);
+        slow->next=NULL;
+        left = sortList(head);
+        return merge(left,right);
     }
 };
