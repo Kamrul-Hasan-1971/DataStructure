f0d04bc5de26f4755646956ef63f58807d62d5f6
Kamrul_Hasan <48267671+Kamrul-Hasan-1971@users.noreply.github.com>
2020-11-26 08:11:54 +0600

    Update Mo's algo.cpp

--- Mo's algo.cpp
+++ Mo's algo.cpp
@@ -1,65 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define  ll   int
-const ll maxn = 100005;
-ll K, ans[maxn], a[maxn], sum,cnt[maxn];
+#define  ll   long long int
+const ll MX = 100005;
+ll K, ans[MX], a[MX], sum,cnt[MX];
 struct Query
 {
-    int index, L, R;
+    ll indx, L, R;
     bool operator<(const Query& other)
     {
-        return L/K < other.L/K ||
-               (L/K == other.L/K && R<other.R);
+        return L/K < other.L/K || (L/K == other.L/K && R<other.R);
     }
 
-}query[maxn];
+}query[MX];
 
 void add(ll indx)
 {
     ll x = ++cnt[a[indx]];
-    if(x==1)
-        sum++;
+    if(x==1) sum++;
 }
 void del(ll indx)
 {
     ll x = --cnt[a[indx]];
-    if(x==0)
-        sum--;
+    if(x==0) sum--;
 }
 
 int main()
 {
     ll T,j;
-    scanf("%d",&T);
+    scanf("%lld",&T);
     for( j = 1 ; j<= T ; j++)
     {
         ll i, n, q, L=1, R=0;
-        scanf("%d%d",&n,&q);
+        scanf("%lld%lld",&n,&q);
         K = sqrt(n);
-        for(i=1; i<=n; i++)
-            scanf("%d",&a[i]);
+        for(i=1; i<=n; i++) scanf("%lld",&a[i]);
         for(i=1; i<=q; i++)
         {
-            scanf("%d%d",&query[i].L,&query[i].R);
-            query[i].index = i;
+            scanf("%lld%lld",&query[i].L,&query[i].R);
+            query[i].indx = i;
         }
         sort(query+1, query+q+1);
         sum=0;
         for(i=1; i<=q; i++)
         {
-            while(R<query[i].R)
-                add(++R);
-            while(L<query[i].L)
-                del(L++);
-            while(R>query[i].R)
-                del(R--);
-            while(L>query[i].L)
-                add(--L);
-            ans[query[i].index] = sum;
+            while(R<query[i].R) add(++R);
+            while(L<query[i].L) del(L++);
+            while(R>query[i].R) del(R--);
+            while(L>query[i].L) add(--L);
+            ans[query[i].indx] = sum;
         }
-        printf("Case %d:\n",j);
-        for(int i=1; i<=q; i++)
-            printf("%d\n",ans[i]);
+        printf("Case %lld:\n",j);
+        for(ll i=1; i<=q; i++) printf("%lld\n",ans[i]);
         memset(cnt,0,sizeof cnt);
     }
     return 0;
