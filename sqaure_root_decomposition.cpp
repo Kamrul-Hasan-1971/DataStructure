c07b8246de737ff26b3e4c9059f4ad7be6a2f1f5
Kamrul_Hasan <48267671+Kamrul-Hasan-1971@users.noreply.github.com>
2020-11-26 07:09:59 +0600

    Update sqaure_root_decomposition.cpp

--- sqaure_root_decomposition.cpp
+++ sqaure_root_decomposition.cpp
@@ -1,83 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define           ll                 long long int
-
-ll segment[10000],input[100000];
-ll preprocess( ll n)
+#define           ll            long long int
+#define           MX            100005
+ll seg[MX],input[MX],seg_size;
+ll build( ll n)
 {
-    ll current_segment = -1;
-    ll segment_size = sqrt(n);
-
-    for (ll i=0; i<n; i++)
-    {
-        if (i % segment_size == 0)
-        {
-            current_segment++;
-        }
-        segment[current_segment] += input[i];
-    }
-    return segment_size;
+    for (ll i=0; i<n; i++) seg[i/seg_size] += input[i];
 }
-
-ll query(ll segment_size, ll l,ll r)
+ll query(ll l,ll r)
 {
     ll sum = 0;
-
-    while (l < r && l % segment_size != 0)
+    while (l < r && l % seg_size != 0) sum += input[l++];
+    while (l + seg_size <= r)
     {
-        sum += input[l];
-        l++;
+        sum += seg[l / seg_size];
+        l += seg_size;
     }
-    while (l + segment_size <= r)
-    {
-        sum += segment[l / segment_size];
-        l += segment_size;
-    }
-    while (l<=r)
-    {
-        sum += input[l];
-        l++;
-    }
+    while (l<=r) sum += input[l++];
     return sum;
 }
-
-void update(ll segment_size, ll i, ll val)
+void update(ll i, ll val)
 {
-    ll segment_no = i / segment_size;
-    segment[segment_no] -= input[i];
-    segment[segment_no] += val;
-    input[i] = val;
+    ll seg_no = i / seg_size;
+    //seg[seg_no] -= input[i];
+    seg[seg_no] += val;
+    input[i] += val;
 }
-
-
 int main()
 {
     ll  i, j, n, q, a, l, r,indx, val,type ;
-
     cin >> n >> q ;
-
-    for( i = 0 ; i < n ; i++ )
-    {
-        cin >> input[i] ;
-    }
-    ll sz = preprocess(n);
-
+    seg_size = sqrt(n);
+    for( i = 0 ; i < n ; i++ ) cin >> input[i] ;
+    build(n);
     for( i = 0 ; i < q ; i++ )
     {
         cin >> type;
         if( type == 0 )
         {
             cin>> indx >> val ;
-            indx--;
-            update(sz,indx,val);
+            update(--indx,val);
         }
         else
         {
             cin >> l >> r  ;
-            l--;
-            r--;
-            cout<<query(sz,l,r)<<endl;
+            cout<<query(--l,--r)<<endl;
         }
     }
     return 0 ;
