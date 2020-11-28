cd5705f1bee237c6c6533013f9944fe6338d3c01
Kamrul_Hasan <48267671+Kamrul-Hasan-1971@users.noreply.github.com>
2020-11-28 09:20:24 +0600

    Update MaxSubArraySum_solution_print(O(N)).cpp

--- MaxSubArraySum_solution_print(O(N)).cpp
+++ MaxSubArraySum_solution_print(O(N)).cpp
@@ -1,34 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-ll ara[100005],n;
+#define ll long long int
+vector<ll>v1;
 ll fun()
 {
-	ll mx = LLONG_MIN, now = 0,
-	start =0, endd = 0, s=0;
-
-	for (ll i=1; i<=n; i++ )
-	{
-		now += ara[i];
-		if (mx < now)
-		{
-			mx = now;
-			//start = s;
-			//endd = i;
-		}
-		if (now < 0)
-		{
-			now = 0;
-			//s = i+1;
-		}
-	}
-    return mx ;
+    ll mx = LLONG_MIN, now = 0, start =0, endd = 0, s=0;
+    for (ll i=0; i< v1.size() ; i++ )
+    {
+        now += v1[i];
+        if (mx < now)
+        {
+            mx = now;
+            //start = s;
+            //endd = i;
+        }
+        if (now < 0)
+        {
+            now = 0;
+            //s = i + 1;
+        }
+    }
+    //cout << "Starting "<< start<< endl << "Ending "<< endd << endl;
+    return mx;
 }
+
 int main()
 {
-    ll i , j ;
+    ll i, j, n, a;
     cin >> n ;
-    for( i = 1 ; i <= n ; i++) cin >> ara[i];
+    for( i = 0 ; i < n ; i++)
+    {
+        cin >> a;
+        v1.push_back(a);
+    }
     ll max_sum = fun();
     cout<<max_sum<<endl;
     return 0;
