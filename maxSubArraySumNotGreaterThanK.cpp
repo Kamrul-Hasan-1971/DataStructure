38cceb5e286f84726626891b1e856595cb89a168
Kamrul_Hasan <48267671+Kamrul-Hasan-1971@users.noreply.github.com>
2022-08-27 16:31:55 +0600

    Add files via upload

---
+++ maxSubArraySumNotGreaterThanK.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define  ll             long long int
+
+ll maxSubArraySumNotGreaterThanK(vector<ll>&arr, ll K)
+{
+	set<ll> cum_set;
+	cum_set.insert(0);
+	ll max_sum = LLONG_MIN, cSum = 0;
+	for (ll i = 0; i < arr.size(); i++) {
+		cSum += arr[i];
+		auto it = cum_set.lower_bound(cSum - K);
+		if (it != cum_set.end()) max_sum = max(max_sum, cSum - *it);
+		cum_set.insert(cSum);
+	}
+	return max_sum;
+}
+
+int main()
+{
+    vector<ll>arr = { 5, -2, 6, 3, -5 , 20};
+	ll K = 15;
+	cout << maxSubarraySumLessThanOrEqualK(arr, K)<<endl;
+    return 0 ;
+}
